40fa2ea704cdd8efb6b68eb4260d066049805f1f
Intisy <finn@birich.de>
2026-04-27 10:52:09 +0200

    Update github-gradle syntax

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     java
     id("com.gradleup.shadow") version "9.3.2"
-    id("io.github.intisy.github-gradle") version "1.3.8"
+    id("io.github.intisy.github-gradle") version "1.8.2.1"
 }
 
 group = "dev.j3fftw"
@@ -10,10 +10,9 @@ description = "LiteXpansion is a Slimefun addon inspired by Industrial Craft 2."
 
 github {
     accessToken = System.getenv("GITHUB_TOKEN") ?: ""
-}
-
-publishGithub {
-    tag = System.getenv("GITHUB_REF_NAME")
+    publish {
+        tag = System.getenv("GITHUB_REF_NAME")
+    }
 }
 
 java {
@@ -33,7 +32,7 @@ repositories {
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
     compileOnly("com.google.code.findbugs:jsr305:3.0.2")
-    githubCompileOnly("Slimefun5:Slimefun5:v5.0.3")
+    "githubCompileOnly"("Slimefun5:Slimefun5:v5.0.3")
 
     // Shaded
     implementation("org.bstats:bstats-bukkit:3.0.2")
